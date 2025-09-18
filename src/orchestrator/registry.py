86a6ddb0d3ca03c9fdeff91b7d58bb2d6a263c4f
Dufour <quentin.dufour@epfprojets.com>
2025-09-18 21:01:52 +0200

    feat: add weather tool

--- src/orchestrator/registry.py
+++ src/orchestrator/registry.py
@@ -4,9 +4,11 @@ from typing import List
 
 # Tools concrets
 from src.agents.search.tools import build_search_tool
+from src.agents.weather.tools import get_weather_by_city
 
 def build_all_tools() -> List[BaseTool]:
     tools: List[BaseTool] = []
     # Web search
     tools.append(build_search_tool())
+    tools.append(get_weather_by_city)
     return tools
